943241dc4911c42b6c8d2f58290fd63291f34ba6
Tristan <75189508+realTristan@users.noreply.github.com>
2021-09-16 16:14:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ class Claimer():
     usernames = queue.Queue()
     headers = {'Ubi-AppId': "2c2d31af-4ee4-4049-85dc-00dc74aef88f","Ubi-RequestedPlatformType": "uplay","user-agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B179 Safari/7534.48.3"}
     checked_count = 0; error_count = 0
-    proxies = {'https://': "http://"+random.choice(open("data/proxies.txt", "r").readlines())}
 
     def create_account(self, user):
         email = f"{user}-" + ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(7))
         password = ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_letters) for _ in range(15))
+        proxies = {"https":f'http://{random.choice(open("proxies.txt", "r").readlines())}',"http":f'http://{random.choice(open("proxies.txt", "r").readlines())}'}
         body = {
             'age': None,
             "dateOfBirth": "1989-01-18T00:00:00.00000Z",
@@ -24,7 +24,7 @@ class Claimer():
             'legalOptinsKey': 'eyJ2dG91IjoiNC4wIiwidnBwIjoiNC4wIiwidnRvcyI6IjIuMSIsImx0b3UiOiJlbi1HQiIsImxwcCI6ImVuLUdCIiwibHRvcyI6ImVuLUdCIn0'
         }
         with requests.Session() as session:
-            r = session.post("https://public-ubiservices.ubi.com/v3/users", headers=self.headers, json=body)
+            r = session.post("https://public-ubiservices.ubi.com/v3/users", proxies=proxies, headers=self.headers, json=body)
             if r.status_code == 200:
                 with open('data/claimed.txt', "a") as f:
                     f.write(f'{user} | {email}:{password}\n')
@@ -45,10 +45,11 @@ class Claimer():
 
     def main(self):
         while not self.usernames.empty():
-            ctypes.windll.kernel32.SetConsoleTitleW(f"Checked: {self.checked_count+1} | Errors: {self.error_count}")
             name = self.usernames.get(); self.usernames.put(name)
+            ctypes.windll.kernel32.SetConsoleTitleW(f"Checked: {self.checked_count+1} | Errors: {self.error_count}")
+            proxies = {"https":f'http://{random.choice(open("proxies.txt", "r").readlines())}',"http":f'http://{random.choice(open("proxies.txt", "r").readlines())}'}
             with requests.Session() as session:
-                r = session.get(f'https://public-ubiservices.ubi.com/v2/profiles?nameOnPlatform={name}&platformType=uplay',headers=self.headers)
+                r = session.get(f'https://public-ubiservices.ubi.com/v2/profiles?nameOnPlatform={name}&platformType=uplay',headers=self.headers, proxies=proxies)
                 if r.status_code == 200:
                     if len(r.json()['profiles']) == 0:
                         print(Fore.GREEN + Style.BRIGHT + f'[+] {name}\n')
